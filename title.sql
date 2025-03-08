9f0908c4f87d5c987c915807d08042ab6ef26260
cameronotoole44 <cameronotoole404@gmail.com>
2025-03-08 17:29:38 -0500

    title INSERT query

---
+++ title.sql
@@ -0,0 +1,13 @@
+INSERT INTO title (title_name) VALUES
+('King of the Seven Kingdoms'),
+('Queen Consort'),
+('Prince of Dragonstone'),
+('Princess of Dragonstone'),
+('Lord of Dragonstone'),
+('Lady of Dragonstone'),
+('Prince'),
+('Princess'),
+('Queen Regnant'),
+('Maester'),
+('Septa'),
+('Archmaester');
