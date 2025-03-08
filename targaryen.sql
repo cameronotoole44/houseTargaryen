2d957f8b28fecb77f130b09d438f987d6fbab5fe
cameronotoole44 <cameronotoole404@gmail.com>
2025-03-08 16:34:00 -0500

    relevant lineage query

---
+++ targaryen.sql
@@ -0,0 +1,126 @@
+INSERT INTO targaryen (name, full_name, birth_date, death_date, reign_start, reign_end) VALUES
+-- pre-conquest
+('Aenar', 'Aenar Targaryen', 'Before 138 BC', 'After 114 BC', NULL, NULL),
+('Gaemon', 'Gaemon Targaryen', 'Before 114 BC', 'After 101 BC', NULL, NULL),
+('Daenys', 'Daenys Targaryen', 'Before 126 BC', 'After 101 BC', NULL, NULL),
+('Aegon', 'Aegon Targaryen (son of Gaemon)', '114 BC', '80 BC', NULL, NULL),
+('Elaena', 'Elaena Targaryen', '114 BC', '80 BC', NULL, NULL),
+('Maegon', 'Maegon Targaryen', '102 BC', '68 BC', NULL, NULL),
+('Aerys', 'Aerys Targaryen', '101 BC', '67 BC', NULL, NULL),
+('Aelyx', 'Aelyx Targaryen', '89 BC', '55 BC', NULL, NULL),
+('Baelon', 'Baelon Targaryen (son of Aerys)', '88 BC', '54 BC', NULL, NULL),
+('Daemion', 'Daemion Targaryen', '87 BC', '53 BC', NULL, NULL),
+('Aerion', 'Aerion Targaryen', '75-41 BC', '27-2 BC', NULL, NULL),
+
+-- Aegon I’s generation
+('Visenya', 'Visenya Targaryen', '29 BC', '44 AC', NULL, NULL),
+('Aegon I', 'Aegon Targaryen I', '27 BC', '37 AC', '1 AC', '37 AC'),
+('Rhaenys', 'Rhaenys Targaryen', '25-26 BC', '10 AC', NULL, NULL),
+
+-- Aenys I and Maegor I’s generation
+('Aenys I', 'Aenys Targaryen I', '7 AC', '42 AC', '37 AC', '42 AC'),
+('Maegor I', 'Maegor Targaryen I', '12 AC', '48 AC', '42 AC', '48 AC'),
+
+-- Aenys I’s children
+('Rhaena', 'Rhaena Targaryen', '23 AC', '73 AC', NULL, NULL),
+('Aegon', 'Aegon Targaryen (the Uncrowned)', '26 AC', '43 AC', NULL, NULL),
+('Viserys', 'Viserys Targaryen (son of Aenys)', '29 AC', '44 AC', NULL, NULL),
+('Jaehaerys I', 'Jaehaerys Targaryen I', '34 AC', '103 AC', '48 AC', '103 AC'),
+('Alysanne', 'Alysanne Targaryen', '36 AC', '100 AC', NULL, NULL),
+('Vaella', 'Vaella Targaryen', '39 AC', '39 AC', NULL, NULL),
+
+-- Jaehaerys I and Alysanne’s children
+('Aegon', 'Aegon Targaryen (son of Jaehaerys)', '51 AC', '51 AC', NULL, NULL),
+('Daenerys', 'Daenerys Targaryen (daughter of Jaehaerys)', '53 AC', '60 AC', NULL, NULL),
+('Aemon', 'Aemon Targaryen (son of Jaehaerys)', '55 AC', '92 AC', NULL, NULL),
+('Baelon', 'Baelon Targaryen (son of Jaehaerys)', '57 AC', '101 AC', NULL, NULL),
+('Alyssa', 'Alyssa Targaryen', '60 AC', '84 AC', NULL, NULL),
+('Maegelle', 'Maegelle Targaryen', '62 AC', '96 AC', NULL, NULL),
+('Vaegon', 'Vaegon Targaryen', '63 AC', 'After 101 AC', NULL, NULL),
+('Daella', 'Daella Targaryen', '64 AC', '82 AC', NULL, NULL),
+('Saera', 'Saera Targaryen', '67 AC', 'After 101 AC', NULL, NULL),
+('Viserra', 'Viserra Targaryen', '71 AC', '87 AC', NULL, NULL),
+('Gaemon', 'Gaemon Targaryen (son of Jaehaerys)', '73 AC', '73 AC', NULL, NULL),
+('Valerion', 'Valerion Targaryen', '77 AC', '78 AC', NULL, NULL),
+('Gael', 'Gael Targaryen', '80 AC', '99 AC', NULL, NULL),
+
+-- Jaehaerys I grandchildren
+('Rhaenys', 'Rhaenys Targaryen (daughter of Aemon)', '74 AC', '129 AC', NULL, NULL),
+('Viserys I', 'Viserys Targaryen I', '77 AC', '129 AC', '103 AC', '129 AC'),
+('Daemon', 'Daemon Targaryen', '81 AC', '130 AC', NULL, NULL),
+
+-- Viserys I’s children and grandchildren
+('Rhaenyra', 'Rhaenyra Targaryen', '97 AC', '130 AC', '129 AC', '130 AC'),
+('Aegon II', 'Aegon Targaryen II', '107 AC', '131 AC', '129 AC', '131 AC'),
+('Helaena', 'Helaena Targaryen', '109 AC', '130 AC', NULL, NULL),
+('Aemond', 'Aemond Targaryen', '110 AC', '130 AC', NULL, NULL),
+('Daeron', 'Daeron Targaryen (son of Viserys I)', '114 AC', '130 AC', NULL, NULL),
+
+-- Rhaenyra’s children
+('Jacaerys', 'Jacaerys Velaryon', '114 AC', '130 AC', NULL, NULL),
+('Lucerys', 'Lucerys Velaryon', '115 AC', '129 AC', NULL, NULL),
+('Joffrey', 'Joffrey Velaryon', '117 AC', '130 AC', NULL, NULL),
+('Aegon III', 'Aegon Targaryen III', '120 AC', '157 AC', '131 AC', '157 AC'),
+('Viserys II', 'Viserys Targaryen II', '122 AC', '172 AC', '171 AC', '172 AC'),
+('Visenya', 'Visenya Targaryen (daughter of Rhaenyra)', '129 AC', '129 AC', NULL, NULL),
+
+-- Daemon’s children
+('Baela', 'Baela Targaryen', '116 AC', 'After 136 AC', NULL, NULL),
+('Rhaena', 'Rhaena Targaryen (daughter of Daemon)', '116 AC', 'After 136 AC', NULL, NULL),
+
+-- Aegon II’s children
+('Jaehaerys', 'Jaehaerys Targaryen (son of Aegon II)', '123 AC', '129 AC', NULL, NULL),
+('Jaehaera', 'Jaehaera Targaryen', '123 AC', '133 AC', NULL, NULL),
+('Maelor', 'Maelor Targaryen', '127 AC', '130 AC', NULL, NULL),
+
+-- Aegon III’s children
+('Daeron I', 'Daeron Targaryen I', '143 AC', '161 AC', '157 AC', '161 AC'),
+('Baelor I', 'Baelor Targaryen I', '144 AC', '171 AC', '161 AC', '171 AC'),
+('Daena', 'Daena Targaryen', '145 AC', 'After 171 AC', NULL, NULL),
+('Rhaena', 'Rhaena Targaryen (daughter of Aegon III)', '147 AC', 'After 171 AC', NULL, NULL),
+('Elaena', 'Elaena Targaryen', '150 AC', 'After 220 AC', NULL, NULL),
+
+-- Viserys II’s children
+('Aegon IV', 'Aegon Targaryen IV', '135 AC', '184 AC', '172 AC', '184 AC'),
+('Aemon', 'Aemon the Dragonknight', '136 AC', '178-183 AC', NULL, NULL),
+('Naerys', 'Naerys Targaryen', '138 AC', '179-184 AC', NULL, NULL),
+
+-- Aegon IV’s children (Main Line and Notable Bastards)
+('Daeron II', 'Daeron Targaryen II', '153 AC', '209 AC', '184 AC', '209 AC'),
+('Daenerys', 'Daenerys Targaryen (daughter of Aegon IV)', '172 AC', 'After 184 AC', NULL, NULL),
+('Daemon', 'Daemon Blackfyre', '170 AC', '196 AC', NULL, NULL),
+
+-- Daeron II’s children
+('Baelor', 'Baelor Breakspear', '170 AC', '209 AC', NULL, NULL),
+('Aerys I', 'Aerys Targaryen I', '172-177 AC', '221 AC', '209 AC', '221 AC'),
+('Rhaegel', 'Rhaegel Targaryen', '173-178 AC', '215 AC', NULL, NULL),
+('Maekar I', 'Maekar Targaryen I', '174-179 AC', '233 AC', '221 AC', '233 AC'),
+
+-- Maekar I’s children
+('Daeron', 'Daeron Targaryen (son of Maekar)', '190-193 AC', '221 AC', NULL, NULL),
+('Aerion', 'Aerion Targaryen', '191-194 AC', '232 AC', NULL, NULL),
+('Aemon', 'Aemon Targaryen (Maester)', '198 AC', '300 AC', NULL, NULL),
+('Aegon V', 'Aegon Targaryen V', '200 AC', '259 AC', '233 AC', '259 AC'),
+('Daella', 'Daella Targaryen (daughter of Maekar)', 'After 198 AC', 'After 233 AC', NULL, NULL),
+('Rhae', 'Rhae Targaryen', 'After 198 AC', 'After 233 AC', NULL, NULL),
+
+-- Aegon V’s children
+('Duncan', 'Duncan Targaryen', '220-224 AC', '259 AC', NULL, NULL),
+('Jaehaerys II', 'Jaehaerys Targaryen II', '225 AC', '262 AC', '259 AC', '262 AC'),
+('Shaera', 'Shaera Targaryen', '226 AC', 'After 259 AC', NULL, NULL),
+('Daeron', 'Daeron Targaryen (son of Aegon V)', '228 AC', '251 AC', NULL, NULL),
+('Rhaelle', 'Rhaelle Targaryen', '229 AC', 'After 259 AC', NULL, NULL),
+
+-- Jaehaerys II’s children
+('Aerys II', 'Aerys Targaryen II', '244 AC', '283 AC', '262 AC', '283 AC'),
+('Rhaella', 'Rhaella Targaryen', '245 AC', '284 AC', NULL, NULL),
+
+-- Aerys II’s children
+('Rhaegar', 'Rhaegar Targaryen', '259 AC', '283 AC', NULL, NULL),
+('Viserys', 'Viserys Targaryen', '276 AC', '298 AC', NULL, NULL),
+('Daenerys', 'Daenerys Targaryen', '284 AC', 'Alive', NULL, NULL),
+
+-- Rhaegar’s children
+('Rhaenys', 'Rhaenys Targaryen (daughter of Rhaegar)', '280 AC', '283 AC', NULL, NULL),
+('Aegon', 'Aegon Targaryen (son of Rhaegar)', '281 AC', '283 AC', NULL, NULL),
+('Jon', 'Jon Snow (Aegon Targaryen)', '283 AC', 'Alive', NULL, NULL);
